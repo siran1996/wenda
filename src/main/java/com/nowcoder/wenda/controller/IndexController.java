d192945b748247cd648fb28ef0b8f3172bc65444
siran1996 <527916897@qq.com>
2019-04-24 00:37:37 +0800

    thymeleaf

--- src/main/java/com/nowcoder/wenda/controller/IndexController.java
+++ src/main/java/com/nowcoder/wenda/controller/IndexController.java
@@ -1,10 +1,10 @@
 package com.nowcoder.wenda.controller;
+import com.nowcoder.wenda.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.Map;
+import java.util.*;
 
 
 //@RestController
@@ -26,10 +26,29 @@ public class IndexController {
         return String.format("Profile Page of %s %d,t:%d,k:%s",groupID,userID,type,key);
     }
 
-
     @RequestMapping(path = "/helloHtml")
     public String helloHtml(Map<String,Object> map) {
         map.put("hello","from TemplateController.helloHtml");
         return"/helloHtml";
+    }
+
+    @RequestMapping(path = "/helloModel")
+    public String helloModel(Model model) {
+        model.addAttribute("value1","vvv1");
+
+        List<String> colors= Arrays.asList(new String[]{"RED","GREEN","BLUE"});
+        model.addAttribute("colors",colors);
+
+        Map<String,String> map =new HashMap<>();
+        for(int i=0;i<4;i++)
+        {
+            map.put(String.valueOf(i),String.valueOf(i*i));
+        }
+        model.addAttribute("map",map);
+
+        model.addAttribute("user",new User("Siran"));
+
+        return"/helloModel";
+
     }
 }
