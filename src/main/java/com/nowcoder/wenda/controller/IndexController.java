ff4886180b4b939bc7c3262b0edc031023c61851
siran1996 <527916897@qq.com>
2019-04-23 17:34:13 +0800

    th

--- src/main/java/com/nowcoder/wenda/controller/IndexController.java
+++ src/main/java/com/nowcoder/wenda/controller/IndexController.java
@@ -1,18 +1,28 @@
 package com.nowcoder.wenda.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.Map;
 
 
-
-@RestController
+//@RestController
 @Controller
 public class IndexController {
 
-        @RequestMapping(value = "hello")
+        @RequestMapping(value = "hello",method = {RequestMethod.GET})
         public String hello() {
-        return new Date() + " : 欢迎您!";
+            return new Date() + " : 欢迎您!";
+        }
+
+
+        @RequestMapping(path = "/helloHtml")
+        public String helloHtml(Map<String,Object> map) {
+            map.put("hello","from TemplateController.helloHtml");
+            return"/helloHtml";
     }
 }
