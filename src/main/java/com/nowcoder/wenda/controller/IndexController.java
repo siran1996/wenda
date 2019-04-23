b923be4d8c03047044c464e9f5b07cf945e51611
siran1996 <527916897@qq.com>
2019-04-23 22:44:33 +0800

    pathvir

--- src/main/java/com/nowcoder/wenda/controller/IndexController.java
+++ src/main/java/com/nowcoder/wenda/controller/IndexController.java
@@ -1,10 +1,7 @@
 package com.nowcoder.wenda.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.Map;
@@ -14,15 +11,25 @@ import java.util.Map;
 @Controller
 public class IndexController {
 
-        @RequestMapping(value = "hello",method = {RequestMethod.GET})
-        public String hello() {
+    @RequestMapping(value = "hello",method = {RequestMethod.GET})
+    public String hello() {
             return new Date() + " : 欢迎您!";
         }
 
+    @RequestMapping(value ="/profile/{groupID}/{userID}")
+    public String profile(@PathVariable("userID") int userID,
+                          @PathVariable("groupID") String groupID,
+                          @RequestParam(value = "type",defaultValue = "1") int type,
+                          @RequestParam(value = "key",defaultValue = "123",required = false) String key
+    )
+    {
+        return String.format("Profile Page of %s %d,t:%d,k:%s",groupID,userID,type,key);
+    }
 
-        @RequestMapping(path = "/helloHtml")
-        public String helloHtml(Map<String,Object> map) {
-            map.put("hello","from TemplateController.helloHtml");
-            return"/helloHtml";
+
+    @RequestMapping(path = "/helloHtml")
+    public String helloHtml(Map<String,Object> map) {
+        map.put("hello","from TemplateController.helloHtml");
+        return"/helloHtml";
     }
 }
