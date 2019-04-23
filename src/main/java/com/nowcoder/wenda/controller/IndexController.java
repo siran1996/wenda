f41d79f0c1e09fe0f1272748775211f30292a756
siran1996 <527916897@qq.com>
2019-04-23 22:32:05 +0800

    pathvariable

--- src/main/java/com/nowcoder/wenda/controller/IndexController.java
+++ src/main/java/com/nowcoder/wenda/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.nowcoder.wenda.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
@@ -11,8 +13,21 @@ import java.util.Date;
 @Controller
 public class IndexController {
 
-        @RequestMapping(value = "hello")
-        public String hello() {
+    @RequestMapping(value = {"hello","/"})
+    public String hello() {
         return new Date() + " : 欢迎您!";
     }
+
+    @RequestMapping(value ="/profile/{groupID}/{userID}")
+    public String profile(@PathVariable("userID") int userID,
+                          @PathVariable("groupID") String groupID,
+                          @RequestParam(value = "type",defaultValue = "1") int type,
+                          @RequestParam(value = "key",defaultValue = "123",required = false) String key
+    )
+    {
+        return String.format("Profile Page of %s %d,t:%d,k:%s",groupID,userID,type,key);
+    }
+
+
+
 }
