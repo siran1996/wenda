0f69389007f2db932e95a373dcd0b079bd3b85e6
siran1996 <527916897@qq.com>
2019-06-28 01:35:10 +0800

    拦截器重写

--- src/main/java/com/nowcoder/wenda/configuration/WendaWebConfiguration.java
+++ src/main/java/com/nowcoder/wenda/configuration/WendaWebConfiguration.java
@@ -7,13 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by nowcoder on 2016/7/3.
  */
 @Configuration
-public class WendaWebConfiguration extends WebMvcConfigurationSupport {
+public class WendaWebConfiguration implements WebMvcConfigurer {
     @Autowired
     PassportInterceptor passportInterceptor;
 
@@ -24,6 +25,6 @@ public class WendaWebConfiguration extends WebMvcConfigurationSupport {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(passportInterceptor);
         registry.addInterceptor(loginRequiredInterceptor).addPathPatterns("/user/*");
-        super.addInterceptors(registry);
+        WebMvcConfigurer.super.addInterceptors(registry);
     }
 }
